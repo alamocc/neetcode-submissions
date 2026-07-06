7ca5177eab21e48b68e0175ac2854f73e3d7699f
alamocc <angelmorilla2005@gmail.com>
2026-07-06 19:28:34 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        		if(s.length() != t.length())
+			return false;
+		
+		HashMap<Character, Integer> countS = new HashMap<>();
+		HashMap<Character, Integer> countT = new HashMap<>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(!countS.containsKey(s.charAt(i))) {
+				countS.put(s.charAt(i), 0);
+			}
+			countS.put(s.charAt(i), countS.get(s.charAt(i))+1);
+			
+			if(!countT.containsKey(t.charAt(i))) {
+				countT.put(t.charAt(i), 0);
+			}
+			countT.put(t.charAt(i), countT.get(t.charAt(i))+1);
+		}
+		return countS.equals(countT);
+    }
+}
